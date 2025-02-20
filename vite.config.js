42112915aca1c061373548158ba0b34633c5d7d7
jizzyone <ritchi21.serebryak@gmail.com>
2025-02-20 13:30:47 +0500

    Temp fix for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Inventory/',
+  // base: '/Inventory/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
